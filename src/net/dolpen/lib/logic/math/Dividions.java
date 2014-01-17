a14f89ffa412a5f8985bb395226e2533eebb8cf3
dolpen <dolpen@dolpen.net>
2014-01-17 16:20:49 +0900

    modpowをつくる

--- src/net/dolpen/lib/logic/math/Dividions.java
+++ src/net/dolpen/lib/logic/math/Dividions.java
@@ -21,4 +21,56 @@ public class Dividions {
         return ans;
     }
 
+    /**
+     * denomの逆数の小数表現における循環節の長さを得る
+     *
+     * @param denom 分母
+     * @return 小数表現における循環節の長さを得る
+     */
+    public static int getRecurringLength(int denom) {
+        int t = denom;
+        while (denom % 2 == 0) denom /= 2;
+        while (denom % 5 == 0) denom /= 5;
+        for (int i = 1; i < t; i++) {
+            if (modPow(10, i, denom) == 1) return i;
+        }
+        return 0;
+    }
+
+    /**
+     * xのp乗をdで割ったときの余りを求める (非再帰)
+     *
+     * @param x
+     * @param p
+     * @param d
+     * @return (x^p)%d
+     */
+    public static int modPow(int x, int p, int d) {
+        int r = 1;
+        for (int i = Integer.SIZE - 1; i >= 0; i--) {
+            r *= r;
+            if ((p & 1 << i) > 0)
+                r *= x;
+            r %= d;
+        }
+        return r;
+    }
+
+    /**
+     * xのp乗をdで割ったときの余りを求める（再帰）
+     *
+     * @param x
+     * @param p
+     * @param d
+     * @return (x^p)%d
+     */
+    public static int modPow2(int x, int p, int d) {
+        if (p == 0) return 1;
+        if (p == 1) return x;
+        int tmp = modPow(x, p / 2, d);
+        tmp = (tmp * tmp) % d;
+        if (p % 2 == 1) tmp = (tmp * x) % d;
+        return tmp;
+    }
+
 }
